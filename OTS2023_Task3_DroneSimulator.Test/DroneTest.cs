cd01f72c144f03620d8b619356b64720ef80aae4
L-26\Andrej <katin@uns.ac.rs>
2023-03-26 10:18:33 +0200

    Add project files.

---
+++ OTS2023_Task3_DroneSimulator.Test/DroneTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS2023_Task3_DroneSimulator.Test
+{
+    public class DroneTest
+    {
+        private Drone drone;
+
+        [SetUp]
+        public void SetUp()
+        {
+            int[] coordinates = { 30, 0, 30 };
+            int[] boundaries = { 50, 50, 50 };
+            drone = new Drone(coordinates, boundaries);
+        }
+
+        [Test]
+        public void Drone_MoveUp_SuccessfullMovingUp()
+        {
+            // ARRANGE
+            int expectedYCoordinate = 1;
+
+            // ACT
+            drone.MoveUp();
+
+            // ASSERT
+            Assert.AreEqual(expectedYCoordinate, drone.coordinates[1]);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            drone = null;
+        }
+    }
+}
